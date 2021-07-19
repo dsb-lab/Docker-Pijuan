d6aea23c21a72eee85fc749e02dd5ecffcd9d68b
Gabriel Torregrosa <g.torregrosa@outlook.com>
2021-07-19 11:27:58 +0200

    R packages installed

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Docker file documents in order to make a docker that reproduces the conditions of the single cell analysis of [Pijuan-Sata et al. (2018)](https://github.com/MarioniLab/EmbryoTimecourse2018).
+Docker file documents in order to make a docker that reproduces the conditions of the single cell analysis of [Pijuan-Sala et al. (2018)](https://github.com/MarioniLab/EmbryoTimecourse2018).
+
+The analysis reproduces from the step 4 on.
+
+# Preparation
+For the installation, you can build the docker from the github source using
+
+```
+./build_git
+```
+
+for downloading the data, use
+
+```
+./download_data
+```
+
+# Run
+
+Once prepared, you will have an image in the docker file called `pijuan`. For an automatic run, you can just call,
+
+```
+./run
+```
+
+## Customizing the run
+
+If you open the bash file `run` you see that it tries to open the pijuan container using Firefox. You can simply change that line of code opening the channel with any other browser that you may have in the computer.
--- install_packages.R
+++ install_packages.R
@@ -1,33 +1,18 @@
-install.packages("devtools")
-# install.packages("BiocManager")
+# install.packages("devtools")
+install.packages("BiocManager")
 
 #Uploading from CRAN
-# install.packages("https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.2-14.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/cowplot/cowplot_0.9.3.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/scales/scales_0.5.0.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/irlba/irlba_2.3.2.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/knitr/knitr_1.20.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/viridis/viridis_0.5.1.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/gtable/gtable_0.2.0.tar.gz") #For ggplot2 correct installation
-# install.packages("https://cran.r-project.org/src/contrib/Archive/digest/digest_0.6.15.tar.gz") #For ggplot2 correct installation
-install.packages("https://cran.r-project.org/src/contrib/Archive/isoband/isoband_0.1.0.tar.gz")
-devtools::install_version("ggplot2",version="3.3.0",repos="https://cran.r-project.org")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/ggplot2/ggplot2_3.3.0.tar.gz", repos=NULL, type="source")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/rmarkdown/rmarkdown_1.10.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/viridisLite/viridisLite_0.3.0.tar.gz")
-
-# install.packages("https://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_0.12.18.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/lattice/lattice_0.20-35.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/prettyunits/prettyunits_1.0.2.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/assertthat/assertthat_0.2.0.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/rprojroot/rprojroot_1.3-2.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/digest/digest_0.6.15.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/plyr/plyr_1.8.4.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/backports_1.1.2/backports_1.1.2.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_0.12.18.tar.gz")
-# install.packages("https://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_0.12.18.tar.gz")
+install.packages("https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.2-14.tar.gz")
+install.packages("cowplot")
+install.packages("https://cran.r-project.org/src/contrib/Archive/scales/scales_0.5.0.tar.gz")
+install.packages("https://cran.r-project.org/src/contrib/Archive/irlba/irlba_2.3.2.tar.gz")
+install.packages("https://cran.r-project.org/src/contrib/Archive/knitr/knitr_1.20.tar.gz")
+install.packages("viridis")
+install.packages("ggplot2")
+install.packages("https://cran.r-project.org/src/contrib/Archive/rmarkdown/rmarkdown_1.10.tar.gz")
+install.packages("https://cran.r-project.org/src/contrib/Archive/viridisLite/viridisLite_0.3.0.tar.gz")
 
 #Uploading from Bioconductor
-# BiocManager::install(version="3.8") #Closest version matching packages from the notebook
-# BiocManager::install("biomaRt")
-# BiocManager::install("BiocStyle")
+BiocManager::install(version="3.8") #Closest version matching packages from the notebook
+BiocManager::install("biomaRt")
+BiocManager::install("BiocStyle")
