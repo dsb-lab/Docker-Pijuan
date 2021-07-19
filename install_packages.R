b894a390cdb75919545de64f1a7b47ef6c79b5e6
Gabriel Torregrosa <g.torregrosa@outlook.com>
2021-07-19 10:00:20 +0200

    Splicit install of biocinductor packages

--- install_packages.R
+++ install_packages.R
@@ -1,14 +1,33 @@
 install.packages("devtools")
-install.packages("BiocManager")
+# install.packages("BiocManager")
 
-# devtools::install_version("Matrix",version="1.2-14", source = "https://cran.r-project.org")
-BiocManager::install("cowplot",version="0.9")
-# BiocManager::install("scales",version="0.5.0")
-# BiocManager::install("irlba",version="2.3.2")
-#devtools::install_version("knitr",version="1.20")
-# BiocManager::install("viridis",version="0.5.1")
-# devtools::install_version("ggplot2",version="3.0.0")
-#devtools::install_version("rmarkdown",version="1.10")
-# BiocManager::install("viridisLite",version="0.3.0")
-# BiocManager::install("biomaRt",version="2.37.3")
-# 
+#Uploading from CRAN
+# install.packages("https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.2-14.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/cowplot/cowplot_0.9.3.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/scales/scales_0.5.0.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/irlba/irlba_2.3.2.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/knitr/knitr_1.20.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/viridis/viridis_0.5.1.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/gtable/gtable_0.2.0.tar.gz") #For ggplot2 correct installation
+# install.packages("https://cran.r-project.org/src/contrib/Archive/digest/digest_0.6.15.tar.gz") #For ggplot2 correct installation
+install.packages("https://cran.r-project.org/src/contrib/Archive/isoband/isoband_0.1.0.tar.gz")
+devtools::install_version("ggplot2",version="3.3.0",repos="https://cran.r-project.org")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/ggplot2/ggplot2_3.3.0.tar.gz", repos=NULL, type="source")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/rmarkdown/rmarkdown_1.10.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/viridisLite/viridisLite_0.3.0.tar.gz")
+
+# install.packages("https://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_0.12.18.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/lattice/lattice_0.20-35.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/prettyunits/prettyunits_1.0.2.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/assertthat/assertthat_0.2.0.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/rprojroot/rprojroot_1.3-2.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/digest/digest_0.6.15.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/plyr/plyr_1.8.4.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/backports_1.1.2/backports_1.1.2.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_0.12.18.tar.gz")
+# install.packages("https://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_0.12.18.tar.gz")
+
+#Uploading from Bioconductor
+# BiocManager::install(version="3.8") #Closest version matching packages from the notebook
+# BiocManager::install("biomaRt")
+# BiocManager::install("BiocStyle")
